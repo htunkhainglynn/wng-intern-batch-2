60b6f3468b4b92d6fb4365db0a303f006e3a138d
mtkyaw04 <mtkyaw04@gmail.com>
2026-06-08 14:12:56 +0630

    Edit: pom.xml

--- payment-service/src/main/java/org/wavemoney/payment/api/service/impl/TransactionServiceImpl.java
+++ payment-service/src/main/java/org/wavemoney/payment/api/service/impl/TransactionServiceImpl.java
@@ -48,7 +48,6 @@ public class TransactionServiceImpl implements TransactionService {
     private final WalletService walletService;
 
     @Override
-    @Transactional
     public TransactionResponse cashIn(CashInRequest request) {
 
         validateDifferentWallet(request);
@@ -127,7 +126,6 @@ public class TransactionServiceImpl implements TransactionService {
         WalletResponse wallet = walletService.getWalletByPhone(userPhone);
         Double newBalance = wallet.balance() + request.amount();
         updateBalanceByPhone(userPhone, newBalance);
-        throw BusinessLogicException.business("TEST_ATOMIC", "Testing atomic.");
     }
 
     private void updateBalanceByPhone(String phone, Double balance){
